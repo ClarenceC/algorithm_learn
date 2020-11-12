b3d0fb06009dbc0e7e328d09f1ff6d9b5c9e8010
Clarence.C <tong901@163.com>
2020-11-12 10:58:44 +0800

    手动实现bubble

--- BubbleSort/bubble.js
+++ BubbleSort/bubble.js
@@ -1,2 +1,17 @@
 // 手动实现冒泡
 
+var arr = [2, 7, 3, 5, 6, 9, 1, 1, 0, 10]
+
+function bubble (array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) { // 每个数都循环一遍，需要循环两遍
+      if (array[j] > array[j+1]) {
+        [array[j], array[j+1]] = [array[j+1], array[j]]
+      }
+    }
+  }
+  return array
+}
+
+console.log(bubble(arr))
+
