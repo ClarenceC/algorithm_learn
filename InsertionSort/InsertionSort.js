3ff38193fb75cac60ebafd68396a6fa013dc13e7
Clarence.C <tong901@163.com>
2020-11-26 14:00:47 +0800

    InsertionSort

--- InsertionSort/InsertionSort.js
+++ InsertionSort/InsertionSort.js
@@ -2,16 +2,17 @@ var arr = [2, 7, 3, 5, 6, 9, 1, 1, 0, 10]
 
 function insertionSort (array) {
   const { length } = array
-  let temp
+  let current
 
+  // 默认第一个元素已经排序
   for (let i = 1; i < length; i++) {
-    let j = i
-    current = array[i]
-    while (j > 0 && array[j - 1] > current) {
-      array[j] = array[j - 1]
+    let j = i // j 索引等于 i
+    current = array[i] // 获取当前值
+    while (j > 0 && array[j - 1] > current) { // 如果该元素小于前一个元素
+      array[j] = array[j - 1] // 全部顺序移一位
       j--
     }
-    array[j] = current
+    array[j] = current // 直到找到合适的位置再放进去
   }
   return array
 }
