6a6006db4441cbfefac7a1e115d627f1ee9f8220
Clarence.C <tong901@163.com>
2021-01-08 11:10:21 +0800

    计数排序

--- CountingSort/CountingSort.js
+++ CountingSort/CountingSort.js
@@ -4,13 +4,16 @@ var arr = [2, 7, 3, 5, 6, 9, 1, 1, 0, 10]
 function countingSort(array) {
   if (array.length < 2) { return array }
   let maxValue = 0
+  // 获取最大值
   array.forEach(item => {
     if (item > maxValue) {
       maxValue = item
     }
   })
 
+  // 分桶
   const counts = new Array(maxValue + 1)
+  // 按值的索引加入桶
   array.forEach(item => {
     if (!counts[item]) {
       counts[item] = 0
@@ -18,6 +21,7 @@ function countingSort(array) {
     counts[item]++
   })
 
+  // 读桶
   let sortedIndex = 0
   counts.forEach((count, index) => {
     while(count > 0) {
